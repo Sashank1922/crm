3f4d460ea2bec35655c8e35f7514e691f48fe85e
Sashamk1922 <sashank.kethamreddy@gmail.com>
2024-11-15 18:46:37 +0530

    Initial commit

---
+++ mini-crm/models/customer.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  totalSpending: { type: Number, default: 0 },
+  numberOfVisits: { type: Number, default: 0 },
+  lastVisit: { type: Number, default: 0 },  // no of days back customer visited
+  age:{type:Number,default:0}
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
+
